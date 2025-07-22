722f89fc1a90f8823f432c85a4cf7391b32fef45
Arek <arkadiuszcieszynski8@gmail.com>
2025-07-22 15:02:42 +0200

    Initial Commit

---
+++ demo/src/test/java/com/arek/praktyki/PraktykiProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arek.praktyki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktykiProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
